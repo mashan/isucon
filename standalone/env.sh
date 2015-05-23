62b7edec3b4149dcd767d99a9060773dcb7aef9f
mashan <masakazusaito94@gmail.com>
2015-05-23 05:53:40 +0000

    Mofified for node v0.12.3

--- standalone/env.sh
+++ standalone/env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-USERNAME=tagomoris
-USER_HOME=/Users/tagomoris
-NODE_VERSION=v0.4.11
+USERNAME=vagrant
+USER_HOME=/home/vagrant
+NODE_VERSION=v0.12.3
 
 export PATH=$PATH:$USER_HOME/.nvm/$NODE_VERSION/bin
 export NODE_PATH=$NODE_PATH:$USER_HOME/node_modules/
