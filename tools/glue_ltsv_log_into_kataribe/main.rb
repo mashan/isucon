b3b366f2b5ebb29b9039d54ed5190cac9572864e
mashan <masakazusaito94@gmail.com>
2015-08-30 11:46:46 +0900

    例外処理を入れておく

--- tools/glue_ltsv_log_into_kataribe/main.rb
+++ tools/glue_ltsv_log_into_kataribe/main.rb
@@ -1,16 +1,20 @@
 require 'ltsv'
 require 'pp'
 
-while line = STDIN.gets
-  parsed = LTSV.parse(line).last
-  log = parsed[:remote_addr] + " -" + " " + "-" + " " + 
-    "[" + parsed[:time] + "]" + ' ' +
-    '"' + parsed[:request_method] + ' ' +  parsed[:uri] + '"' + ' ' +
-    parsed[:status] + ' ' +
-    parsed[:body_bytes_sent] + ' ' +
-    parsed[:client_referer] + ' ' + 
-    parsed[:client_useragent] + ' ' + 
-    parsed[:client_forwardedfor] + ' ' + 
-    parsed[:request_time]
-  puts log
+while line = STDIN.gets.chomp
+  begin 
+    parsed = LTSV.parse(line).last
+    log = parsed[:remote_addr] + " -" + " " + "-" + " " + 
+      "[" + parsed[:time] + "]" + ' ' +
+      '"' + parsed[:request_method] + ' ' +  parsed[:uri] + '"' + ' ' +
+      parsed[:status] + ' ' +
+      parsed[:body_bytes_sent] + ' ' +
+      parsed[:client_referer] + ' ' + 
+      parsed[:client_useragent] + ' ' + 
+      parsed[:client_forwardedfor] + ' ' + 
+      parsed[:request_time]
+    puts log
+  rescue
+    warn 'parse error. #{line}'
+  end
 end
